86fe83c5699d0b159894cd9efce1088edb3f8239
Rick <rick-2007@outlook.com>
2025-07-08 15:47:43 +0200

    clock changed to hour minute

--- js/clock.js
+++ js/clock.js
@@ -2,12 +2,17 @@ function displayCurrentTime(divSelector) {
   const clockDiv = document.querySelector(divSelector);
   if (!clockDiv) return;
 
+  let timeSpan = clockDiv.querySelector("span");
+  if (!timeSpan) {
+    timeSpan = document.createElement("span");
+    clockDiv.appendChild(timeSpan);
+  }
+
   function updateTime() {
     const now = new Date();
     const hh = now.getHours().toString().padStart(2, "0");
     const mm = now.getMinutes().toString().padStart(2, "0");
-    const ss = now.getSeconds().toString().padStart(2, "0");
-    clockDiv.innerText = `${hh}:${mm}:${ss}`;
+    timeSpan.innerText = `${hh}:${mm}`;
   }
 
   updateTime();
