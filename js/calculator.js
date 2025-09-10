5095207e030f00492a289d3eb9fc5e561f903da1
Rick <rick-2007@outlook.com>
2025-09-10 18:12:53 +0200

    calculator fully done

--- js/calculator.js
+++ js/calculator.js
@@ -1,77 +1,113 @@
 const resultDisplay = document.querySelector(".result-display");
 
+function appendToDisplay(value) {
+  if (resultDisplay.innerHTML === "0") {
+    resultDisplay.innerHTML = value;
+  } else {
+    resultDisplay.innerHTML += value;
+  }
+}
+
+function setDisplay(value) {
+  resultDisplay.innerHTML = value;
+}
+
+function getDisplay() {
+  return resultDisplay.innerHTML;
+}
+
 document.querySelector("#clearBtn").addEventListener("click", () => {
-  resultDisplay.innerText = "0";
+  setDisplay("0");
 });
 
-document.querySelector("#oneBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "1";
-  } else {
-    resultDisplay.innerText += "1";
+for (let i = 0; i <= 9; i++) {
+  document.querySelector(`#${["zero","one","two","three","four","five","six","seven","eight","nine"][i]}Btn`).addEventListener("click", () => {
+    if (getDisplay() === "0") {
+      setDisplay(i.toString());
+    } else {
+      appendToDisplay(i.toString());
+    }
+  });
+}
+
+document.querySelector("#dotBtn").addEventListener("click", () => {
+  const display = getDisplay();
+  const parts = display.split(/[\+\-\×÷%√]|<i[^>]+><\/i>/g);
+  if (!parts[parts.length - 1].includes(".")) {
+    appendToDisplay(".");
   }
 });
 
-document.querySelector("#twoBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "2";
+document.querySelector("#addBtn").addEventListener("click", () => appendOperator("+"));
+document.querySelector("#subtractBtn").addEventListener("click", () => appendOperator("-"));
+document.querySelector("#multiplyBtn").addEventListener("click", () => appendOperator("×"));
+document.querySelector("#divideBtn").addEventListener("click", () => appendOperator("÷"));
+document.querySelector("#moduleBtn").addEventListener("click", () => appendOperator("%"));
+
+document.querySelector("#sqrtBtn").addEventListener("click", () => {
+  const sqrtIcon = '<i class="fa-regular fa-square-root"></i>';
+  const display = getDisplay();
+  if (display === "0" || /[\+\-\×÷%]$/.test(display)) {
+    setDisplay(sqrtIcon);
   } else {
-    resultDisplay.innerText += "2";
+    setDisplay(sqrtIcon + display);
   }
 });
 
-document.querySelector("#threeBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "3";
+document.querySelector("#deleteBtn").addEventListener("click", () => {
+  let display = getDisplay();
+  if (display.startsWith('<i class="fa-regular fa-square-root"></i>')) {
+    display = display.replace(/^<i class="fa-regular fa-square-root"><\/i>/, "");
   } else {
-    resultDisplay.innerText += "3";
+    display = display.slice(0, -1);
+  }
+  if (display === "" || display === '<i class="fa-regular fa-square-root"></i>') {
+    setDisplay("0");
+  } else {
+    setDisplay(display);
   }
 });
 
-document.querySelector("#fourBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "4";
-  } else {
-    resultDisplay.innerText += "4";
+document.querySelector("#equalsBtn").addEventListener("click", () => {
+  let expression = getDisplay();
+
+  expression = expression.replace(/<i class="fa-regular fa-square-root"><\/i>/g, "√");
+  expression = expression.replace(/×/g, "*").replace(/÷/g, "/");
+
+  if (expression.startsWith("√")) {
+    const num = parseFloat(expression.slice(1));
+    if (!isNaN(num)) {
+      setDisplay(Math.sqrt(num).toString());
+      return;
+    }
+  }
+
+  if (expression.startsWith('<i class="fa-regular fa-square-root"></i>')) {
+    const num = parseFloat(expression.replace(/^<i class="fa-regular fa-square-root"><\/i>/, ""));
+    if (!isNaN(num)) {
+      setDisplay(Math.sqrt(num).toString());
+      return;
+    }
+  }
+
+  expression = expression.replace(/%/g, "/100*");
+
+  try {
+    let result = eval(expression);
+    if (result === undefined || isNaN(result)) result = "0";
+    setDisplay(result.toString());
+  } catch {
+    setDisplay("Error");
   }
 });
 
-document.querySelector("#fiveBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "5";
+// Prevent double operators
+function appendOperator(op) {
+  const display = getDisplay();
+  if (display === "0") return;
+  if (/[+\-×÷%]$/.test(display)) {
+    setDisplay(display.slice(0, -1) + op);
   } else {
-    resultDisplay.innerText += "5";
+    appendToDisplay(op);
   }
-});
-
-document.querySelector("#sixBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "6";
-  } else {
-    resultDisplay.innerText += "6";
-  }
-});
-
-document.querySelector("#sevenBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "7";
-  } else {
-    resultDisplay.innerText += "7";
-  }
-});
-
-document.querySelector("#eightBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "8";
-  } else {
-    resultDisplay.innerText += "8";
-  }
-});
-
-document.querySelector("#nineBtn").addEventListener("click", () => {
-  if (resultDisplay.innerText === "0") {
-    resultDisplay.innerText = "9";
-  } else {
-    resultDisplay.innerText += "9";
-  }
-});
+}
